97b425605ec3cbea37b67b5f00d1ce9916c5f494
Daerim Han <gkseofla7@naver.com>
2022-01-28 21:38:47 +0900

    프로젝트 설정

---
+++ Engine/EnginePch.h
@@ -0,0 +1,29 @@
+#pragma once
+
+// 각종 include
+#include <iostream>
+#include <windows.h>
+#include <tchar.h>
+#include <memory>
+#include <string>
+#include <vector>
+#include <array>
+#include <list>
+#include <map>
+using namespace std;
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// 각종 typedef
+using int8 = __int8;
+using int16 = __int16;
+using int32 = __int32;
+using int64 = __int64;
+using uint8 = unsigned __int8;
+using uint16 = unsigned __int16;
+using uint32 = unsigned __int32;
+using uint64 = unsigned __int64;
+
+
+void HelloEngine();
